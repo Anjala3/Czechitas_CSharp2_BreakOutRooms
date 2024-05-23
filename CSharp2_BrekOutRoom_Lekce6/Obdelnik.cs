bf1c6d78f007bc11387304a46fd02957a90861a4
Anna <komorova.an@gmail.com>
2024-05-23 11:27:49 +0200

    Doplneny Break Out room 2

--- CSharp2_BrekOutRoom_Lekce6/Obdelnik.cs
+++ CSharp2_BrekOutRoom_Lekce6/Obdelnik.cs
@@ -2,23 +2,73 @@
 {
     public class Obdelnik
     {
-        public int Sirka;
-        public int Vyska;
+        public int Sirka { get; private set; }
+        public int Vyska { get; private set; }
+        public string Jmeno { get; private set; }
 
-        public Obdelnik(int sirka, int vyska)
+        private int pomocnaObsah;
+        private bool obsahUpraveny = false;
+
+        public int Obsah
+        {
+            get { return pomocnaObsah; }
+            private set
+            {
+                if (value == 0)
+                {
+                    pomocnaObsah = 1;
+                    obsahUpraveny = true;
+                }
+                else
+                {
+                    pomocnaObsah = value;
+                }
+            }
+        }
+
+        public int Obvod
+        {
+            get { return 2 * (Sirka + Vyska); }
+        }
+
+        public Obdelnik(int sirka, int vyska, string jmeno)
         {
             Sirka = sirka;
             Vyska = vyska;
+            Jmeno = jmeno;
+            Obsah = Sirka * Vyska;
         }
 
-        public Obdelnik(int sirka) : this(sirka, sirka)
-        {
-
-        }
+        public Obdelnik(int sirka) : this(sirka, sirka, "Ctverec") { }
 
         public void VypisInformace()
         {
-            Console.WriteLine($"Sirka obedlniku je: {Sirka}, vyska obedlniku je {Vyska}");
+            Console.WriteLine($"Sirka obdelniku {Jmeno} je: {Sirka}, jeho vyska je {Vyska}");
+        }
+
+        public void ZvetsiObdelnik()
+        {
+            Sirka *= 2;
+            Vyska *= 2;
+            Obsah = Sirka * Vyska;
+            Console.WriteLine($"Nove rozmery obdelniku {Jmeno} - Sirka: {Sirka}, Vyska: {Vyska}");
+        }
+
+        public void VypisObvod()
+        {
+            Console.WriteLine($"Obvod obdelniku {Jmeno} je: {Obvod}");
+        }
+
+        public void VypisObsah()
+        {
+            if (obsahUpraveny)
+            {
+                Console.WriteLine($"Obsah nemuze byt nula! Obsah obdelniku {Jmeno} byl nastaven na 1.");
+            }
+            else
+            {
+                Console.WriteLine($"Obsah obdelniku {Jmeno} je: {Obsah}");
+            }
         }
     }
 }
--- CSharp2_BrekOutRoom_Lekce6/Program.cs
+++ CSharp2_BrekOutRoom_Lekce6/Program.cs
@@ -4,23 +4,50 @@
     {
         static void Main(string[] args)
         {
+            // BREAKOUT ROOM 1
             //  Vytvoř třídu Obdelnik (tak, aby byla v samostatnem souboru Obdelnik.cs), která bude mít dva fieldy: Sirka a Vyska.
             //  Vytvoř konstruktor třídy Obdelnik s parametry sirka a vyska.
             //  Přidej do třídy ještě jeden typ konstruktoru pro speciální případ obdelníku(čtverec) s jedním parametrem, který bude volat první konstruktor.
             //  Obdelniku vytvorte funkci Vypis informace, ktery vypise vysku a sirku.
             //  Vytvoř několik instancí třídy Obdelnik (například 2) a vypiš jejich vlastnosti pomoci metody VypisParameteryObdelniku.
 
-            Obdelnik prvniObdelnik = new Obdelnik(12, 24);
-            Obdelnik druhyObdelnik = new Obdelnik(3, 50);
+            Obdelnik prvniObdelnik = new Obdelnik(0, 24, "Prvni obdelnik");
+            Obdelnik druhyObdelnik = new Obdelnik(3, 50, "Druhy obdelnik");
             Obdelnik tretiObdelnik = new Obdelnik(14);
 
             prvniObdelnik.VypisInformace();
             druhyObdelnik.VypisInformace();
             tretiObdelnik.VypisInformace();
+
+            // BREAKOUT ROOM 2
+            /* Vyuzij tridu Obdelnik z prvniho BR.
+               Prepis fieldy Sirka a Vyska na properties.
+               Nastavte properties tak, aby je nebylo mozne zmenit po vytvoreni instance.  
+               Vytvorte property Obsah, ktera umozni ziskat pomocnaObsah obdelniku.
+               Vytvorte property Obvod, ktera umozni ziskat obvod obdelniku.
+               Vytvorte funkci Zvetsi, ktera zvetsi obdelnik o sirku a vysku.
+               Zajistete, aby nebylo mozne vytvorit obdelnik o obsahu 0 (vypiste hlasku a nastavte hodnotu na 1).
+               Napiste program, ktery vytvori obdelnik, vypise jeho velikosti, pomocnaObsah a obvod.
+            */
+
+
+            prvniObdelnik.VypisObsah();
+            prvniObdelnik.VypisObvod();
+            druhyObdelnik.VypisObsah();
+            druhyObdelnik.VypisObvod();
+            tretiObdelnik.VypisObsah();
+            tretiObdelnik.VypisObvod();
+
+            prvniObdelnik.ZvetsiObdelnik();
+            druhyObdelnik.ZvetsiObdelnik();
+            tretiObdelnik.ZvetsiObdelnik();
+
         }
 
-
     }
+
+
 }
+
 
 
